140219d7d8ef7e7fa5a4a46556935b5daaa17dfc
Rohan Doshi <rohan.doshi02@gmail.com>
2022-03-28 10:34:41 +0530

    Menu Driven Assignment4

--- DSAL/Assignment4.cpp
+++ DSAL/Assignment4.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-#define size 501
+#define size 500
 
 class HashEntry {
 	long int telephoneNo;
@@ -14,17 +14,26 @@ public:
 
 class HashTable {
 	HashEntry hashArray[size];
+	int elements;
 	int hashFunction(long int key) {
 		return key % size;
 	}
 public:
+	HashTable() {
+		elements = 0;
+	}
 	void Insert(long int key, string value) {
+		if (elements == size) {
+			cout << "Hash Table FULL" << endl;
+			return;
+		}
 		int hashIndex = hashFunction(key);
 		int hashIndexCopy = hashIndex;
 		do {
 			if (hashArray[hashIndex].telephoneNo == 0) {
 				hashArray[hashIndex].telephoneNo = key;
 				hashArray[hashIndex].name = value;
+				elements++;
 				break;
 			}
 
@@ -34,6 +43,10 @@ public:
 	}
 
 	void InsertWithReplacement(long int key, string value) {
+		if (elements == size) {
+			cout << "Hash Table FULL" << endl;
+			return;
+		}
 		int hashIndex = hashFunction(key);
 		int hashIndexCopy = hashIndex;
 
@@ -41,6 +54,7 @@ public:
 			if (hashArray[hashIndex].telephoneNo == 0) {
 				hashArray[hashIndex].telephoneNo = key;
 				hashArray[hashIndex].name = value;
+				elements++;
 				break;
 			} else {
 				int hashIndexOfCollision = hashFunction(
@@ -67,8 +81,8 @@ public:
 	void Display() {
 		for (int i = 0; i < size; i++) {
 			if (hashArray[i].telephoneNo != 0) {
-				cout << i << " " << hashArray[i].name << " "
-						<< hashArray[i].telephoneNo << endl;
+				cout << i << "\t" << hashArray[i].telephoneNo << "\t"
+						<< hashArray[i].name << endl;
 			}
 		}
 	}
@@ -82,11 +96,14 @@ public:
 				cout << hashIndex << " " << hashArray[hashIndex].name << " "
 						<< hashArray[hashIndex].telephoneNo << " Collisions: "
 						<< count << endl;
+				return;
 			}
 			hashIndex = (hashIndex + 1) % size;
 			count++;
 
 		} while (hashIndex != hashIndexCopy);
+
+		cout << "Key NOT FOUND!" << endl;
 	}
 	void Update(long int key) {
 		int hashIndex = hashFunction(key);
@@ -98,27 +115,95 @@ public:
 						<< hashArray[hashIndex].telephoneNo << endl;
 				cout << "Enter Name to be Updated: ";
 				string nameUpdate;
-				cin >> nameUpdate;
+				getline(cin, nameUpdate);
 				hashArray[hashIndex].name = nameUpdate;
 				cout << "Entry Updated" << endl;
-				cout << hashIndex << " " << hashArray[hashIndex].name << " "
-						<< hashArray[hashIndex].telephoneNo << endl;
+				cout << hashIndex << " " << hashArray[hashIndex].telephoneNo
+						<< " " << hashArray[hashIndex].name << endl;
+				return;
 			}
 			hashIndex = (hashIndex + 1) % size;
 
 		} while (hashIndex != hashIndexCopy);
+
+		cout << "Key NOT FOUND!" << endl;
 	}
 };
 
 int main() {
 	HashTable t1;
-	t1.Insert(9049987412, "Mukund");
-	t1.Insert(9325712554, "Rohan");
-	t1.Insert(9325612554, "Rohan No2");
-	t1.Insert(9125612554, "Rohan No3");
-	t1.InsertWithReplacement(9125612555, "Rohan No4");
-	t1.Display();
-	t1.Search(9125612554);
-	t1.Search(9325712554);
+	bool continueProgram = true;
+	while (continueProgram) {
+		cout << "Menu" << endl;
+		cout << "1. Add Hash entry" << endl;
+		cout << "2. Add Hash entry with replacement" << endl;
+		cout << "3. Search Key" << endl;
+		cout << "4. Update Value" << endl;
+		cout << "5. Display" << endl;
+		cout << "-1 End" << endl;
+		cout << "Choose operations u want to perform? ";
+		int choice = 0;
+		cin >> choice;
+		cout << endl;
+
+		switch (choice) {
+		case 1: {
+			cout << "Enter key: ";
+			long int key;
+			cin >> key;
+			cout << "Enter value: ";
+			string value = "";
+			cin.ignore();
+			cin.clear();
+			getline(cin, value);
+			t1.Insert(key, value);
+			cout << endl;
+			break;
+		}
+		case 2: {
+			cout << "Enter key: ";
+			long int key;
+			cin >> key;
+			cout << "Enter value: ";
+			string value = "";
+			cin.ignore();
+			cin.clear();
+			getline(cin, value);
+			t1.InsertWithReplacement(key, value);
+			cout << endl;
+			break;
+		}
+		case 3: {
+			cout << "Enter key: ";
+			long int key;
+			cin >> key;
+			t1.Search(key);
+			break;
+		}
+		case 4: {
+			cout << "Enter key: ";
+			long int key;
+			cin >> key;
+			t1.Update(key);
+			cout << endl;
+			break;
+		}
+		case 5: {
+			cout << "Index\tKey\tValue\n";
+			t1.Display();
+			cout << endl;
+			break;
+		}
+		case -1: {
+			continueProgram = false;
+			break;
+		}
+		default: {
+			cout << "INVALID CHOICE" << endl;
+			break;
+		}
+			cout << endl;
+		}
+	}
 	return 0;
 }
