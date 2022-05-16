879718df626bbe33441371831b5fcabfc4a8225b
Rohan Doshi <rohan.doshi02@gmail.com>
2022-05-16 10:21:27 +0530

    Update Assignment10.cpp

--- DSAL/Assignment10.cpp
+++ DSAL/Assignment10.cpp
@@ -1,209 +1,379 @@
 #include <iostream>
 #include <fstream>
 #define file_name "Assignment10.txt"
-#define temp_name "Assignment_temp.txt"
+#define temp_name "temp10.txt"
 using namespace std;
 
 class Student
 {
 private:
-    string roll;
-    string name, address, cl;
+    int rollNum;
+    string name;
+    string address;
+    string division;
 
 public:
+    friend class Database;
     Student()
     {
-        roll = "0";
         name = "";
+        rollNum = -1;
+        division = "";
         address = "";
     }
-
-    Student(string roll, string name, string address, string cl)
+    Student(string name, int roll, string div, string addr)
     {
-        this->address = address;
-        this->cl = cl;
         this->name = name;
-        this->roll = roll;
+        this->rollNum = roll;
+        this->division = div;
+        this->address = addr;
     }
-
-    void display()
-    {
-        cout << name << "  " << address << "  " << cl << "  " << roll << endl;
-    }
-
-    string getRoll()
-    {
-        return roll;
-    }
-
-    string getName()
-    {
-        return name;
-    }
-
-    string getAddress()
-    {
-        return address;
-    }
-
-    string getclass()
-    {
-        return cl;
-    }
+    friend class Database;
 };
 
-class fileOp
+class Database
 {
-    fstream file;
-
 public:
-    void write(string filename, Student st)
+    Student std;
+    void insert()
     {
-        ofstream fout(filename, ios::app);
-        if (!fout)
-        {
-            cout << "Cannot open file for writing" << endl;
-            return;
-        }
-        fout << st.getName() << " " << st.getRoll() << " " << st.getAddress()
-             << " " << st.getclass() << endl;
-        fout.close();
+        ofstream outf;
+        cout << "\nEnter name: ";
+        cin.ignore();
+        getline(cin, std.name);
+        cout << "Enter roll number: ";
+        cin >> std.rollNum;
+        cout << "Enter division: ";
+        cin >> std.division;
+        cout << "Enter address: ";
+        cin.ignore();
+        getline(cin, std.address);
+        outf.open(file_name, ios::app | ios::binary);
+        outf.write((char *)&std, sizeof(std)) << flush;
+        outf.close();
+        cout << "\nSuccessfully added";
     }
 
-    void read(string filename)
+    void Deletion()
     {
-        ifstream fin(filename, ios::in);
-
-        if (!fin)
-        {
-            cerr << "Cannot open file for writing" << endl;
-            return;
-        }
-        int roll = 0;
-        string name = "";
-        string cl = "";
-        string address = "";
-        while (fin >> name >> roll >> address >> cl)
-        {
-            cout << name << "  " << roll << "  " << address << "  " << cl
-                 << endl;
-        }
-    }
-
-    void deleteStudent(string filename, string name)
-    {
+        bool flag = 0;
+        int x;
+        string name;
+        string s;
+        cout << "\nEnter name: ";
+        cin >> name;
         ifstream fin;
-        fin.open(file_name);
-
-        ofstream temp;
-        temp.open(temp_name);
-
-        string line;
-        while (getline(fin, line))
+        ofstream fout;
+        fout.open(temp_name, ios::app | ios::binary);
+        fin.open(file_name, ios::in | ios::binary);
+        while (fin.read((char *)&std, sizeof(std)))
         {
-            if (line.find(name))
-            {
-                temp << line << endl;
-            }
+            if (std.name == name)
+                flag = 1;
+            if (std.name != name)
+                fout.write((char *)&std, sizeof(std)) << flush;
         }
-        temp.close();
-        fin.close();
         remove(file_name);
         rename(temp_name, file_name);
+        cout << (flag ? "\nDeletion Success" : "\nCould not Record");
     }
 
-    void searchStudent(string filename, string name)
+    void Search()
+    {
+        bool flag = 0;
+        string name;
+        cout << "\nEnter name: ";
+        cin >> name;
+        ifstream inf;
+        inf.open(file_name, ios::in | ios::binary);
+        inf.seekg(0);
+        inf.clear();
+        int x;
+        if (inf.is_open())
+        {
+            while (inf.read((char *)&std, sizeof(std)))
+            {
+                if (std.name == name)
+                {
+                    flag = 1;
+                    cout << "\nEntry present";
+                    cout << "\nName: " << std.name;
+                    cout << "\nRoll number: " << std.rollNum;
+                    cout << "\nDivision: " << std.division;
+                    cout << "\nAddress: " << std.address;
+                }
+            }
+            if (!flag)
+                cout << "\nCould not find record";
+            inf.close();
+        }
+        else
+            cout << "\nUnable to open";
+    }
+
+    void Display()
     {
         ifstream fin;
-        fin.open(file_name);
-        int flag = 0;
-        string line;
-
-        while (getline(fin, line))
+        fin.open(file_name, ios::in | ios::binary);
+        if (fin.is_open())
         {
-
-            if (!line.find(name))
+            int i = 1;
+            while (fin.read((char *)&std, sizeof(std)))
             {
-                cout << line << endl;
-                flag = 1;
+                cout << "Student-" << (i++) << endl;
+                cout << "\nName: " << std.name;
+                cout << "\nRoll number: " << std.rollNum;
+                cout << "\nDivision: " << std.division;
+                cout << "\nAddress: " << std.address;
+                cout << endl;
             }
+            fin.close();
         }
-
-        if (flag == 0)
-        {
-            cout << "Record not present\n";
-        }
-
-        fin.close();
+        else
+            cout << "\nCould not open";
     }
-};
+} * file;
 
 int main()
 {
-    fileOp obj;
-    string filename = file_name;
-
-    int choice = 0;
-
-    while (choice != 5)
+    bool menu = 1;
+    file = new Database();
+    while (menu)
     {
-        cout << "**********\n";
-        cout << "1. Add a student\n2. Display all students\n3. Delete a student profile\n4. Search a student profile\n5. Exit\n";
-        cout << "**********\n";
-
-        cout << "Enter your choice: ";
-        cin >> choice;
-
-        switch (choice)
+        int ch;
+        cout << "\n\n1. Insert Record\n2. Search Record\n3. Delete Record\n4. Display Entire File Contents\n What do you want to perform: ";
+        cin >> ch;
+        switch (ch)
         {
         case 1:
-        {
-            string name, address, cl;
-            string roll;
+            file->insert();
+            break;
 
-            cout << "Enter the name of the student: ";
-            cin >> name;
-            cout << "\nEnter the address: ";
-            cin >> address;
-            cout << "\nEnter the division: ";
-            cin >> cl;
-            cout << "\nEnter the roll number: ";
-            cin >> roll;
-
-            Student st = Student(roll, name, address, cl);
-            obj.write(filename, st);
-        }
-        break;
         case 2:
-        {
-            obj.read(filename);
-        }
-        break;
+            file->Search();
+            break;
+
         case 3:
-        {
-            string roll;
-            cout << "Enter the name you want to delete: ";
-            cin >> roll;
-            obj.deleteStudent(filename, roll);
-        }
-        break;
+            file->Deletion();
+            break;
+
         case 4:
-        {
-            string roll;
-            cout << "Enter the name you want to search: ";
-            cin >> roll;
-            obj.searchStudent(filename, roll);
-        }
-        break;
-        case 5:
-        {
-            cout << "Exit" << endl;
-        }
-        break;
+            file->Display();
+            break;
+
         default:
-            cout << "Enter a valid choice";
             break;
         }
     }
     return 0;
 }
+
+// #include <iostream>
+// #include <fstream>
+// #define file_name "Assignment10.txt"
+// #define temp_name "Assignment_temp.txt"
+// using namespace std;
+
+// class Student
+// {
+// private:
+//     string roll;
+//     string name, address, cl;
+
+// public:
+//     Student()
+//     {
+//         roll = "0";
+//         name = "";
+//         address = "";
+//     }
+
+//     Student(string roll, string name, string address, string cl)
+//     {
+//         this->address = address;
+//         this->cl = cl;
+//         this->name = name;
+//         this->roll = roll;
+//     }
+
+//     void display()
+//     {
+//         cout << name << "  " << address << "  " << cl << "  " << roll << endl;
+//     }
+
+//     string getRoll()
+//     {
+//         return roll;
+//     }
+
+//     string getName()
+//     {
+//         return name;
+//     }
+
+//     string getAddress()
+//     {
+//         return address;
+//     }
+
+//     string getclass()
+//     {
+//         return cl;
+//     }
+// };
+
+// class fileOp
+// {
+//     fstream file;
+
+// public:
+//     void write(string filename, Student st)
+//     {
+//         ofstream fout(filename, ios::app);
+//         if (!fout)
+//         {
+//             cout << "Cannot open file for writing" << endl;
+//             return;
+//         }
+//         fout << st.getName() << " " << st.getRoll() << " " << st.getAddress()
+//              << " " << st.getclass() << endl;
+//         fout.close();
+//     }
+
+//     void read(string filename)
+//     {
+//         ifstream fin(filename, ios::in);
+
+//         if (!fin)
+//         {
+//             cerr << "Cannot open file for writing" << endl;
+//             return;
+//         }
+//         int roll = 0;
+//         string name = "";
+//         string cl = "";
+//         string address = "";
+//         while (fin >> name >> roll >> address >> cl)
+//         {
+//             cout << name << "  " << roll << "  " << address << "  " << cl
+//                  << endl;
+//         }
+//     }
+
+//     void deleteStudent(string filename, string name)
+//     {
+//         ifstream fin;
+//         fin.open(file_name);
+
+//         ofstream temp;
+//         temp.open(temp_name);
+
+//         string line;
+//         while (getline(fin, line))
+//         {
+//             if (line.find(name))
+//             {
+//                 temp << line << endl;
+//             }
+//         }
+//         temp.close();
+//         fin.close();
+//         remove(file_name);
+//         rename(temp_name, file_name);
+//     }
+
+//     void searchStudent(string filename, string name)
+//     {
+//         ifstream fin;
+//         fin.open(file_name);
+//         int flag = 0;
+//         string line;
+
+//         while (getline(fin, line))
+//         {
+
+//             if (!line.find(name))
+//             {
+//                 cout << line << endl;
+//                 flag = 1;
+//             }
+//         }
+
+//         if (flag == 0)
+//         {
+//             cout << "Record not present\n";
+//         }
+
+//         fin.close();
+//     }
+// };
+
+// int main()
+// {
+//     fileOp obj;
+//     string filename = file_name;
+
+//     int choice = 0;
+
+//     while (choice != 5)
+//     {
+//         cout << "**********\n";
+//         cout << "1. Add a student\n2. Display all students\n3. Delete a student profile\n4. Search a student profile\n5. Exit\n";
+//         cout << "**********\n";
+
+//         cout << "Enter your choice: ";
+//         cin >> choice;
+
+//         switch (choice)
+//         {
+//         case 1:
+//         {
+//             string name, address, cl;
+//             string roll;
+
+//             cout << "Enter the name of the student: ";
+//             cin >> name;
+//             cout << "\nEnter the address: ";
+//             cin >> address;
+//             cout << "\nEnter the division: ";
+//             cin >> cl;
+//             cout << "\nEnter the roll number: ";
+//             cin >> roll;
+
+//             Student st = Student(roll, name, address, cl);
+//             obj.write(filename, st);
+//         }
+//         break;
+//         case 2:
+//         {
+//             obj.read(filename);
+//         }
+//         break;
+//         case 3:
+//         {
+//             string roll;
+//             cout << "Enter the name you want to delete: ";
+//             cin >> roll;
+//             obj.deleteStudent(filename, roll);
+//         }
+//         break;
+//         case 4:
+//         {
+//             string roll;
+//             cout << "Enter the name you want to search: ";
+//             cin >> roll;
+//             obj.searchStudent(filename, roll);
+//         }
+//         break;
+//         case 5:
+//         {
+//             cout << "Exit" << endl;
+//         }
+//         break;
+//         default:
+//             cout << "Enter a valid choice";
+//             break;
+//         }
+//     }
+//     return 0;
+// }
